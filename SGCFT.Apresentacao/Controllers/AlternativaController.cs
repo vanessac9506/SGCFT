9f739feb072cf76b487201270065efa5e3706dd6
Vanessa Cristina <vanessa.c9506@hotmail.com>
2019-10-07 20:30:07 -0300

    criação das demais telas de cadastro

--- SGCFT.Apresentacao/Controllers/AlternativaController.cs
+++ SGCFT.Apresentacao/Controllers/AlternativaController.cs
@@ -1,7 +1,9 @@
-﻿using SGCFT.Dados.Repositorios;
+﻿using SGCFT.Apresentacao.Models;
+using SGCFT.Dados.Repositorios;
 using SGCFT.Dominio.Contratos.Servicos;
 using SGCFT.Dominio.Entidades;
 using SGCFT.Dominio.Servicos;
+using SGCFT.Utilitario;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +22,19 @@ namespace SGCFT.Apresentacao.Controllers
 
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(AlternativaViewModel alternativaViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Alternativa alternativa = alternativaViewModel.ConverterParaDominio();
+                Retorno retorno = _servicoAlternativas.InserirAlternativa(alternativa);
+                ViewBag.Sucesso = retorno.Sucesso;
+                ViewBag.Mensagens = retorno.Mensagens;
+            }
             return View();
         }
     }
---
+++ SGCFT.Apresentacao/Models/AlternativaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using SGCFT.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SGCFT.Apresentacao.Models
+{
+    public class AlternativaViewModel
+    {
+        public int Id { get; set; }
+        public int IdPergunta { get; set; }
+        public Pergunta Pergunta { get; set; }
+        [Required(ErrorMessage = "Informe o texto")]
+        public string Texto { get; set; }
+        [Required(ErrorMessage = "Informe se a alternativa está certa/errada")]
+        public bool CertoErrado { get; set; }
+
+        public Alternativa ConverterParaDominio()
+        {
+            Alternativa alternativa = new Alternativa(this.IdPergunta, this.Texto, this.CertoErrado);
+            return alternativa;
+
+        }
+    }
+}
