6079606c30bd4a44aa62b26f9b84cad0a81205f8
Vanessa Cristina <vanessac9506@gmail.com>
2019-10-22 23:49:43 -0300

    Organizando na pasta

--- SGCFT.Apresentacao/Controllers/UsuarioController.cs
+++ SGCFT.Apresentacao/Controllers/UsuarioController.cs
@@ -14,7 +14,7 @@ using System.Web.Mvc;
 
 namespace SGCFT.Apresentacao.Controllers
 {
-    public class UsuarioController : Controller
+    public class UsuarioController : BaseController
     {
         private readonly IUsuarioServico _servicoUsuarios;
 
